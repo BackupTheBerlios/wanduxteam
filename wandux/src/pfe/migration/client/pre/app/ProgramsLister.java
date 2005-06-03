31458692f8e88678870a26a10dbd1077012b1b4c
cb6 <cb6>
2005-06-03 16:23:05 +0000

    Added a class which will find programs installed on Windows clients, majorly based on extension matching

---
+++ wandux/src/pfe/migration/client/pre/app/ProgramsLister.java
@@ -0,0 +1,17 @@
+/*
+ * Created on 3 juin 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package pfe.migration.client.pre.app;
+
+/**
+ * @author cb6
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class ProgramsLister {
+
+}
