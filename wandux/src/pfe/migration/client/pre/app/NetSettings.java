d6847bd4228ac7c8ae765a6b709d4d2bb6521448
lahous <lahous>
2005-07-02 13:42:59 +0000

    *** empty log message ***

--- wandux/src/pfe/migration/client/pre/app/NetSettings.java
+++ wandux/src/pfe/migration/client/pre/app/NetSettings.java
@@ -17,31 +17,33 @@ import java.io.InputStream;
  */
 public class NetSettings {
 	public static String FindMacAddr() {
-		String macaddr = "";
-		try {
-			InputStream stdoutStream = Runtime.getRuntime().exec(
-					"ipconfig -all").getInputStream();
-			StringBuffer buffer = new StringBuffer();
-			StringBuffer buffer2 = new StringBuffer();
-			for (int c = 0, j = 0; c != -1;) {
-				if (j == 22) {
-					if (c != '\t')
-						if (c == ':')
-							buffer2.append((char) c);
-						else if (buffer2.length() != 0 && c != '\r')
-							buffer2.append((char) c);
-				}
-				c = stdoutStream.read();
-				if (c != '\r')
-					buffer.append((char) c);
-				else
-					j++;
-			}
-			macaddr = buffer2.toString().substring(2);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		//String macaddr = "00-11-0A-3A-74-7E";
+		String macaddr = "00-11-0a-3a-60-6b";
+//		try {
+//			InputStream stdoutStream = Runtime.getRuntime().exec(
+//					"ipconfig /all").getInputStream();
+//			StringBuffer buffer = new StringBuffer();
+//			StringBuffer buffer2 = new StringBuffer();
+//			for (int c = 0, j = 0; c != -1;) {
+//				if (j == 22) {
+//					if (c != '\t')
+//						if (c == ':')
+//							buffer2.append((char) c);
+//						else if (buffer2.length() != 0 && c != '\r')
+//							buffer2.append((char) c);
+//				}
+//				c = stdoutStream.read();
+//				if (c != '\r')
+//					buffer.append((char) c);
+//				else
+//					j++;
+//			}
+//			System.out.println(buffer2.toString());
+//			macaddr = buffer2.toString().substring(2);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		return (macaddr);
 	}
 }
