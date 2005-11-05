f1a150ca47968b52bd861312c29ab632ec338a97
cornflaks <cornflaks>
2005-11-05 18:35:53 +0000

    FsXml

--- wandux/src/pfe/migration/server/ejb/tool/FsXmlst.java
+++ wandux/src/pfe/migration/server/ejb/tool/FsXmlst.java
@@ -10,36 +10,38 @@ import java.io.*;
 
 /**
  * @author CornFlaks
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * 
+ * TODO To change the template for this generated type comment go to Window -
+ * Preferences - Java - Code Style - Code Templates
  */
 public class FsXmlst {
 
-	public FsXmlst(){
-	
-	try {
-		BufferedWriter out = new BufferedWriter(new FileWriter(
-				"utils/FileSystemXml/newfilesystem.xml"));
-		out
-				.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<tree id=\"0\">\n");
-		File[] roots = File.listRoots();
-		for (int i = 1; i < roots.length; i++) {
+	public FsXmlst() {
 
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter(
+					"utils/FileSystemXml/newfilesystem.xml"));
 			out
-					.write("  "
-							+ "<item text=\""
-							+ roots[i]
-							+ "\" id=\""
-							+ roots[i]
-							+ "\" im0=\"leaf.gif\" im1=\"folderOpen.gif\" im2=\"folderClosed.gif\">\n");
-			FsXml child = new FsXml(roots[i].toString(), out);
-			child.listAll();
-			out.write("  </item>\n");
+					.write("<?xml version=\"1.0\" encoding=\"ISO-8859-1\" standalone=\"yes\"?>"
+							+ "\n<tree id=\"0\">\n");
+			File[] roots = File.listRoots();
+			for (int i = 1; i < roots.length; i++) {
+
+				out
+						.write("  "
+								+ "<item text=\""
+								+ roots[i]
+								+ "\" id=\""
+								+ roots[i]
+								+ "\" im0=\"leaf.gif\" im1=\"folderOpen.gif\" im2=\"folderClosed.gif\">\n");
+				FsXml child = new FsXml(roots[i].toString(), out);
+				child.listAll();
+				out.write("  </item>\n");
+			}
+			out.write("</tree>");
+			out.close();
+			System.out.println("terminer");
+		} catch (IOException e) {
 		}
-		out.write("</tree>");
-		out.close();
-		System.out.println("terminer");
-	} catch (IOException e) {}
 	}
 }
