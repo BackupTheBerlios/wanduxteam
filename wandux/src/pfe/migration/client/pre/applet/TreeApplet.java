4489a293ef3489a46dd534b51f68fcd426ae2876
lahous <lahous>
2005-12-09 11:19:53 +0000

    Applet browser de fichier avec checkboxe

---
+++ wandux/src/pfe/migration/client/pre/applet/TreeApplet.java
@@ -0,0 +1,46 @@
+package pfe.migration.client.pre.applet;
+
+import java.applet.Applet;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Scrollbar;
+import java.io.File;
+
+import javax.swing.JTree;
+
+import pfe.migration.client.pre.applet.tree.CheckTreeManager;
+
+import embeded.system.FileSystemModel;
+
+public class TreeApplet extends Applet {
+
+	public void init()
+	{
+		setLayout(new BorderLayout());
+		setBackground(Color.WHITE);
+
+		FileSystemModel fileSystemModel = new FileSystemModel(new File("\\"));
+//		final JTextArea fileDetails = new JTextArea("");
+
+		final JTree fileTree = new JTree(fileSystemModel);
+        new CheckTreeManager(fileTree);
+		add(fileTree, BorderLayout.CENTER);
+
+		// scrollbar
+		// http://forum.java.sun.com/thread.jspa?threadID=422026&messageID=1874240
+		//
+		// http://tools.arlut.utexas.edu/gash2/doc/javadoc/arlut/csd/JTree/treeControl.html
+		
+//		Scrollbar redSlider=new Scrollbar();
+//		redSlider.addAdjustmentListener(fileTree.get)
+//		add(redSlider, BorderLayout.EAST);
+		
+		// add(new JCheckBox(), BorderLayout.CENTER);
+		// add(new TristateCheckBox("euh", null), BorderLayout.CENTER);
+	  	// add(new Label("Vous etes interdit de séjour sur les chats Interneto", Label.CENTER), BorderLayout.CENTER);
+
+		invalidate();
+		validate();
+	}
+	
+}
---
+++ wandux/src/pfe/migration/client/pre/applet/tree/CheckTreeCellRenderer.java
@@ -0,0 +1,98 @@
+package pfe.migration.client.pre.applet.tree;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.event.MouseEvent;
+import java.util.EventObject;
+
+import javax.swing.JCheckBox;
+import javax.swing.JPanel;
+import javax.swing.JTree;
+import javax.swing.event.CellEditorListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeCellEditor;
+import javax.swing.tree.TreeCellRenderer;
+import javax.swing.tree.TreePath;
+
+public class CheckTreeCellRenderer extends JPanel implements TreeCellRenderer{
+    /**
+	 * Comment for <code>serialVersionUID</code>
+	 */
+	private static final long serialVersionUID = 1L;
+	private CheckTreeSelectionModel selectionModel; 
+    private TreeCellRenderer delegate; 
+    private TristateCheckBox checkBox = new TristateCheckBox(); 
+ 
+    public CheckTreeCellRenderer(TreeCellRenderer delegate, CheckTreeSelectionModel selectionModel){ 
+        this.delegate = delegate; 
+        this.selectionModel = selectionModel; 
+        setLayout(new BorderLayout()); 
+        setOpaque(false); 
+        checkBox.setOpaque(false); 
+    } 
+ 
+    public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus){ 
+    	//remove(checkBox);
+    	Component renderer = delegate.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus); 
+ 
+        TreePath path = tree.getPathForRow(row); 
+        if(path!=null){
+            if(selectionModel.isPathSelected(path)) //, true)) 
+                checkBox.setState(Boolean.TRUE); 
+            else 
+                checkBox.setState(selectionModel.isPartiallySelected(path) ? null : Boolean.FALSE); 
+        } 
+        removeAll(); 
+        add(checkBox, BorderLayout.WEST); 
+        add(renderer, BorderLayout.CENTER); 
+//      this.invalidate();
+//      this.validate();
+        return this; 
+    } 
+}  
+
+
+//class RenduComposant implements TreeCellRenderer{
+//    
+//        public Component getTreeCellRendererComponent(JTree tree, Object obj,
+//            boolean selected, boolean expanded, boolean leaf,
+//                int row, boolean hasFocus){
+//        
+//        DefaultMutableTreeNode dmtcr = (DefaultMutableTreeNode)obj;
+//        JCheckBox toto = (JCheckBox)dmtcr.getUserObject();
+//
+//        return toto;
+//    }
+//}
+
+//class EditComposant implements TreeCellEditor{
+//
+//    public void addCellEditorListener(CellEditorListener l){ }
+//    public void cancelCellEditing() { }
+//    public Object getCellEditorValue(){
+//        return this;
+//    }
+//    public boolean isCellEditable(EventObject evt){
+//        if(evt instanceof MouseEvent){
+//            MouseEvent mevt = (MouseEvent) evt;
+//            if (mevt.getClickCount() == 1){
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//
+//    public void removeCellEditorListener(CellEditorListener l){}
+//    public boolean shouldSelectCell(EventObject anEvent){
+//        return true;
+//    }
+//    public boolean stopCellEditing(){
+//            return false;
+//    }
+//    public Component getTreeCellEditorComponent(JTree tree, Object obj, boolean isSelected, boolean expanded, boolean leaf, int row){
+//        DefaultMutableTreeNode dmtn = (DefaultMutableTreeNode)obj;
+//        JCheckBox tata=(JCheckBox)dmtn.getUserObject();
+//        tata.setEnabled(true);
+//        return tata;
+//    }
+//}
