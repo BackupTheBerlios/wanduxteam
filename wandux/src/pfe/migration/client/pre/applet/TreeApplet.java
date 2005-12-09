e73e3eccd489a989598d8291bea7bb99d68e6ae4
lahous <lahous>
2005-12-09 13:52:25 +0000

    Applet browser de fichier avec checkboxe et avec scrollbar :D !!

--- wandux/src/pfe/migration/client/pre/applet/TreeApplet.java
+++ wandux/src/pfe/migration/client/pre/applet/TreeApplet.java
@@ -3,8 +3,11 @@ package pfe.migration.client.pre.applet;
 import java.applet.Applet;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 import java.io.File;
 
+import javax.swing.JScrollPane;
 import javax.swing.JTree;
 
 import pfe.migration.client.pre.applet.system.FileSystemModel;
@@ -12,33 +15,39 @@ import pfe.migration.client.pre.applet.tree.CheckTreeManager;
 
 public class TreeApplet extends Applet {
 
+	private static final long serialVersionUID = 1L;
+
 	public void init()
 	{
 		setLayout(new BorderLayout());
 		setBackground(Color.WHITE);
 
 		FileSystemModel fileSystemModel = new FileSystemModel(new File("\\"));
-//		final JTextArea fileDetails = new JTextArea("");
-
 		final JTree fileTree = new JTree(fileSystemModel);
         new CheckTreeManager(fileTree);
-		add(fileTree, BorderLayout.CENTER);
 
-		// scrollbar
-		// http://forum.java.sun.com/thread.jspa?threadID=422026&messageID=1874240
-		//
-		// http://tools.arlut.utexas.edu/gash2/doc/javadoc/arlut/csd/JTree/treeControl.html
-		
-//		Scrollbar redSlider=new Scrollbar();
-//		redSlider.addAdjustmentListener(fileTree.get)
-//		add(redSlider, BorderLayout.EAST);
-		
-		// add(new JCheckBox(), BorderLayout.CENTER);
-		// add(new TristateCheckBox("euh", null), BorderLayout.CENTER);
-	  	// add(new Label("Vous etes interdit de séjour sur les chats Interneto", Label.CENTER), BorderLayout.CENTER);
+        JScrollPane jsp  = new JScrollPane(fileTree);
+        addAdjustmentListener(jsp);
+        
+        add(jsp, BorderLayout.CENTER);
 
 		invalidate();
 		validate();
 	}
 	
+	private void addAdjustmentListener(JScrollPane jsp)
+	{
+		jsp.getVerticalScrollBar().addAdjustmentListener(new AdjustmentListener()
+		{	public void adjustmentValueChanged(AdjustmentEvent a)
+			{
+				repaint();
+			}
+		});
+		jsp.getHorizontalScrollBar().addAdjustmentListener(new AdjustmentListener()
+		{	public void adjustmentValueChanged(AdjustmentEvent a)
+			{
+				repaint();
+			}
+		});
+	}
 }
