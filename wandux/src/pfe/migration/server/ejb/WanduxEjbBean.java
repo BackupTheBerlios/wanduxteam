085403b6fd6d2c6466f08b0166a7c78b5718a982
lahous <lahous>
2005-12-09 18:11:32 +0000

    j ai vire putconpueurinformation et le getcomputerinforamtion parce que ca pete des erreurs et que personnes ne sais  a quoi ca sert ... si besoin le code est en commentaires

--- wandux/src/pfe/migration/server/ejb/WanduxEjbBean.java
+++ wandux/src/pfe/migration/server/ejb/WanduxEjbBean.java
@@ -115,36 +115,36 @@ public class WanduxEjbBean implements SessionBean
 		return (ComputerInformation)cil.get(ip);
 	}
 	
-	public void putComputerInformation(ComputerInformation ci)
-	{
-		String ip = ci.getIp();
-
-//		useCiList();
-//		cil.add(ip);
-		
-		Transaction transaction;
-		Session session;
-		try {
-			session = HibernateUtil.currentSession();
-			transaction = session.beginTransaction();
-			session.save(ci.gconf);
-			session.save(ci.netconf);
-			session.save(ci.udata);
-			session.save(ci.ieconf);
-			transaction.commit();
-			HibernateUtil.closeSession();
-		} catch (HibernateException e) {
-			e.printStackTrace();
-		}
-		
-//		exemple d'utilisation des threads // TODO a enleve prochainement
-//		WorkQueue wt = new WorkQueue(10);
-//		wt.execute(new SendingData(ci));
-
-		// c est pas pour recuppere le nom du fichier
-		createAdllXmlFile (ci); // a enleve pour que ca puisse etre gere depuis le monitoring
-		createXmlBookmark (ci);
-	}
+//	public void putComputerInformation(ComputerInformation ci)
+//	{ // TODO vire cette merde
+//		String ip = ci.getIp();
+//
+////		useCiList();
+////		cil.add(ip);
+//		
+//		Transaction transaction;
+//		Session session;
+//		try {
+//			session = HibernateUtil.currentSession();
+//			transaction = session.beginTransaction();
+//			session.save(ci.gconf);
+//			session.save(ci.netconf);
+//			session.save(ci.udata);
+//			session.save(ci.ieconf);
+//			transaction.commit();
+//			HibernateUtil.closeSession();
+//		} catch (HibernateException e) {
+//			e.printStackTrace();
+//		}
+//		
+////		exemple d'utilisation des threads // TODO a enleve prochainement
+////		WorkQueue wt = new WorkQueue(10);
+////		wt.execute(new SendingData(ci));
+//
+//		// c est pas pour recuppere le nom du fichier
+//		createAdllXmlFile (ci); // a enleve pour que ca puisse etre gere depuis le monitoring
+//		createXmlBookmark (ci);
+//	}
 
 	public List getLangInformation()
 	{
@@ -158,107 +158,107 @@ public class WanduxEjbBean implements SessionBean
 		return l;
 	}
 
-	public ComputerInformation getComputerInformation(String macaddr) throws RemoteException, HibernateException
-	{
-		List l = null;
-		Iterator i = null;
-		Integer user_id = null;
-		Session session;
-		ComputerInformation ci = new ComputerInformation();
-		
-		session = HibernateUtil.currentSession();
-
-		ci.gconf = new GlobalConf();
-		l = session.find(" from GlobalConf ");
-		i = l.iterator();
-		while (i.hasNext())
-		{
-			GlobalConf gconf = (GlobalConf)i.next();
-			if (gconf != null)
-				if (gconf.getGlobalKey() != null)
-					if (gconf.getGlobalKey().equals(new Integer(macaddr.hashCode())))
-			{
-				ci.gconf.setGlobalDomainName(gconf.getGlobalDomainName());
-				ci.gconf.setGlobalHostname(gconf.getGlobalHostname());
-				ci.gconf.setGlobalKey(gconf.getGlobalKey());
-				break;
-			}
-		}
-		
-		
-		ci.netconf = new NetworkConfig();
-		l = session.find(" from NetworkConfig ");
-		i = l.iterator();
-		while (i.hasNext())
-		{
-			NetworkConfig netconf = (NetworkConfig)i.next();
-			if (netconf != null)
-				if (netconf.getNetworkKey() != null)
-					if (netconf.getNetworkKey().equals(new Integer(macaddr.hashCode())))
-			{
-				ci.netconf.setId(netconf.getId());
-				ci.netconf.setNetworkDhcpEnabled(netconf.getNetworkDhcpEnabled());
-				ci.netconf.setNetworkDnsServer(netconf.getNetworkDnsServer());
-				ci.netconf.setNetworkDnsServer2(netconf.getNetworkDnsServer2());
-				ci.netconf.setNetworkGateway(netconf.getNetworkGateway());
-				ci.netconf.setNetworkInterface(netconf.getNetworkInterface());
-				ci.netconf.setNetworkIpAddress(netconf.getNetworkIpAddress());
-				ci.netconf.setNetworkKey(netconf.getNetworkKey());
-				ci.netconf.setNetworkMacAdress(netconf.getNetworkMacAdress());
-				ci.netconf.setNetworkStatus(netconf.getNetworkStatus());
-				ci.netconf.setNetworkSubnetmask(netconf.getNetworkSubnetmask());
-				break;
-			}
-		}
-
-		
-		ci.udata = new UsersData();
-		l = session.find(" from UsersData ");
-		i = l.iterator();
-		while (i.hasNext())
-		{
-			UsersData udata = (UsersData)i.next();
-			if (udata != null)
-				if (udata.getUserKey() != null)
-					if (udata.getUserKey().equals(new Integer(macaddr.hashCode())))
-			{
-				user_id = new Integer(udata.getUserLogin().hashCode());
-				ci.udata.setUserLogin(udata.getUserLogin());
-				ci.udata.setUserBgimg(udata.getUserBgimg());
-				ci.udata.setUserHome(udata.getUserHome());
-				ci.udata.setUserKey(udata.getUserKey());
-				ci.udata.setUserPass(udata.getUserPass());
-				ci.udata.setUserProxyOverride(udata.getUserProxyOverride());
-				ci.udata.setUserProxyServ(udata.getUserProxyServ());
-				ci.udata.setUserType(udata.getUserType());
-				break;
-			}
-		}
-		
-
-		ci.ieconf = new ParamIe();
-		l = session.find(" from ParamIe ");
-		i = l.iterator();
-		while (i.hasNext())
-		{
-			ParamIe ieconf = (ParamIe)i.next();
-			if (ieconf != null)
-				if (ieconf.getIeParamUserId() != null)
-					if (ieconf.getIeParamUserId().equals(user_id))
-			{
-				ci.ieconf.setId(ieconf.getId());
-				ci.ieconf.setIeParamSaveDirectory(ieconf.getIeParamSaveDirectory());
-				ci.ieconf.setIeParamUserId(ieconf.getIeParamUserId());
-				ci.ieconf.setIeProxyAutoConfigUrl(ieconf.getIeProxyAutoConfigUrl());
-				ci.ieconf.setIeProxyOverride(ieconf.getIeProxyOverride());
-				ci.ieconf.setIeProxyServer(ieconf.getIeProxyServer());
-				break;
-			}
-		}
-		
-		HibernateUtil.closeSession();
-		return ci;
-	}
+//	public ComputerInformation getComputerInformation(String macaddr) throws RemoteException, HibernateException
+//	{
+//		List l = null;
+//		Iterator i = null;
+//		Integer user_id = null;
+//		Session session;
+//		ComputerInformation ci = new ComputerInformation();
+//		
+//		session = HibernateUtil.currentSession();
+//
+//		ci.gconf = new GlobalConf();
+//		l = session.find(" from GlobalConf ");
+//		i = l.iterator();
+//		while (i.hasNext())
+//		{
+//			GlobalConf gconf = (GlobalConf)i.next();
+//			if (gconf != null)
+//				if (gconf.getGlobalKey() != null)
+//					if (gconf.getGlobalKey().equals(new Integer(macaddr.hashCode())))
+//			{
+//				ci.gconf.setGlobalDomainName(gconf.getGlobalDomainName());
+//				ci.gconf.setGlobalHostname(gconf.getGlobalHostname());
+//				ci.gconf.setGlobalKey(gconf.getGlobalKey());
+//				break;
+//			}
+//		}
+//		
+//		
+//		ci.netconf = new NetworkConfig();
+//		l = session.find(" from NetworkConfig ");
+//		i = l.iterator();
+//		while (i.hasNext())
+//		{
+//			NetworkConfig netconf = (NetworkConfig)i.next();
+//			if (netconf != null)
+//				if (netconf.getNetworkKey() != null)
+//					if (netconf.getNetworkKey().equals(new Integer(macaddr.hashCode())))
+//			{
+//				ci.netconf.setId(netconf.getId());
+//				ci.netconf.setNetworkDhcpEnabled(netconf.getNetworkDhcpEnabled());
+//				ci.netconf.setNetworkDnsServer(netconf.getNetworkDnsServer());
+//				ci.netconf.setNetworkDnsServer2(netconf.getNetworkDnsServer2());
+//				ci.netconf.setNetworkGateway(netconf.getNetworkGateway());
+//				ci.netconf.setNetworkInterface(netconf.getNetworkInterface());
+//				ci.netconf.setNetworkIpAddress(netconf.getNetworkIpAddress());
+//				ci.netconf.setNetworkKey(netconf.getNetworkKey());
+//				ci.netconf.setNetworkMacAdress(netconf.getNetworkMacAdress());
+//				ci.netconf.setNetworkStatus(netconf.getNetworkStatus());
+//				ci.netconf.setNetworkSubnetmask(netconf.getNetworkSubnetmask());
+//				break;
+//			}
+//		}
+//
+//		
+//		ci.udata = new UsersData();
+//		l = session.find(" from UsersData ");
+//		i = l.iterator();
+//		while (i.hasNext())
+//		{
+//			UsersData udata = (UsersData)i.next();
+//			if (udata != null)
+//				if (udata.getUserKey() != null)
+//					if (udata.getUserKey().equals(new Integer(macaddr.hashCode())))
+//			{
+//				user_id = new Integer(udata.getUserLogin().hashCode());
+//				ci.udata.setUserLogin(udata.getUserLogin());
+//				ci.udata.setUserBgimg(udata.getUserBgimg());
+//				ci.udata.setUserHome(udata.getUserHome());
+//				ci.udata.setUserKey(udata.getUserKey());
+//				ci.udata.setUserPass(udata.getUserPass());
+//				ci.udata.setUserProxyOverride(udata.getUserProxyOverride());
+//				ci.udata.setUserProxyServ(udata.getUserProxyServ());
+//				ci.udata.setUserType(udata.getUserType());
+//				break;
+//			}
+//		}
+//		
+//
+//		ci.ieconf = new ParamIe();
+//		l = session.find(" from ParamIe ");
+//		i = l.iterator();
+//		while (i.hasNext())
+//		{
+//			ParamIe ieconf = (ParamIe)i.next();
+//			if (ieconf != null)
+//				if (ieconf.getIeParamUserId() != null)
+//					if (ieconf.getIeParamUserId().equals(user_id))
+//			{
+//				ci.ieconf.setId(ieconf.getId());
+//				ci.ieconf.setIeParamSaveDirectory(ieconf.getIeParamSaveDirectory());
+//				ci.ieconf.setIeParamUserId(ieconf.getIeParamUserId());
+//				ci.ieconf.setIeProxyAutoConfigUrl(ieconf.getIeProxyAutoConfigUrl());
+//				ci.ieconf.setIeProxyOverride(ieconf.getIeProxyOverride());
+//				ci.ieconf.setIeProxyServer(ieconf.getIeProxyServer());
+//				break;
+//			}
+//		}
+//		
+//		HibernateUtil.closeSession();
+//		return ci;
+//	}
 
 	// -- monitoring ----------------------------------------------------------------------------- //
 //	private void useCiList ()
