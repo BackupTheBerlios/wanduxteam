bc3f1155effaa2371535d4e225a9641608389e65
cornflaks <cornflaks>
2006-02-04 16:58:00 +0000

    WMI request using jabob variant return good repository

---
+++ wandux/src/pfe/migration/client/pre/service/CimValues.java
@@ -0,0 +1,78 @@
+package pfe.migration.client.pre.service;
+
+import java.util.StringTokenizer;
+
+import com.jacob.com.Dispatch;
+import com.jacob.com.EnumVariant;
+import com.jacob.com.Variant;
+
+public class CimValues {
+
+	public static String TabProperties[];
+
+	public static Dispatch ObjConnect;
+
+	public static String querystring;
+
+	public static int NB_PROPERTIES;
+
+	public static String tablename;
+
+	public static String elementname;
+
+	public CimValues(String Tabprop[], Dispatch activx, String query) {
+
+		// initialisation
+
+		TabProperties = Tabprop;
+		ObjConnect = activx;
+		querystring = query;
+		NB_PROPERTIES = Tabprop.length;
+
+		// decoupage de la querystring
+		StringTokenizer st = new StringTokenizer(query);
+		for (int i = 0; i < 4; i++) {
+			tablename = st.nextToken(" ");
+		}
+
+		st = new StringTokenizer(tablename);
+		for (int i = 0; i < 2; i++) {
+			elementname = st.nextToken("_");
+		}
+	}
+
+	public Variant[] GetValues() {
+
+		Object selectParam = new Variant(querystring);
+		Variant retquery = Dispatch.call(ObjConnect, "ExecQuery", selectParam);
+		Dispatch enumvalues = retquery.toDispatch();
+		EnumVariant penumvalues = new EnumVariant(enumvalues);
+		Variant ElementEnum;
+		Variant[] resultat = new Variant[65535];
+
+		int j = 0;
+		while ((ElementEnum = penumvalues.Next()) != null) {
+			for (int i = 0; i < NB_PROPERTIES; i++) {
+				Dispatch obEnum = ElementEnum.toDispatch();
+				Variant RetValue = Dispatch.call(obEnum, TabProperties[i]);
+				if (!(RetValue.toString().equals("null"))) {
+					if (RetValue.toString().equals("???")) {
+						String test = TabProperties[i] + "(0)";
+						try {
+							RetValue = Dispatch.call(obEnum, test);
+							resultat[j] = RetValue;
+						} catch (Exception e) {
+						}
+					} else {
+						resultat[j] = RetValue;
+					}
+					j++;
+				}
+			}
+		}
+		Variant[] res = new Variant[j - 1];
+		for (int i = 0, k = 0; i < res.length; i++, k++)
+			res[k] = resultat[i];
+		return res;
+	}
+}
