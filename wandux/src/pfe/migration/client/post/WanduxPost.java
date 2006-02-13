ab89fe2a98ade4635fc642c3ee790016f3c4885c
cb6 <cb6>
2006-02-13 19:07:42 +0000

    - groupadd and useradd modification added - added method for mounting temporary samba shares from the wandux file server

--- wandux/src/pfe/migration/client/post/WanduxPost.java
+++ wandux/src/pfe/migration/client/post/WanduxPost.java
@@ -8,8 +8,10 @@ package pfe.migration.client.post;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.rmi.RemoteException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -52,7 +54,7 @@ public class WanduxPost
 			return "";
 		}
 	}
-	
+
 	public WanduxPost (String applicationServerIp)
 	{
 		this.storageServerIp = applicationServerIp;
@@ -70,8 +72,8 @@ public class WanduxPost
 		
 		init();
 		usersGroupsCreation();
-		confWebPrograms();
 		getDataFromStorageServer();
+		confPrograms();
 		
 		ce.EjbClose();
 	}
@@ -101,7 +103,7 @@ public class WanduxPost
 		for (int i = 0; i < ci.udata.length; i++)
 		{
 			try {
-				Process p = Runtime.getRuntime().exec("addgroup " + ci.udata[i].getUserType());
+				Process p = Runtime.getRuntime().exec("groupadd " + ci.udata[i].getUserType());
 				BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
 				this.currentHostname = input.readLine();
 				input.close();
@@ -110,7 +112,7 @@ public class WanduxPost
 			}
 
 			try {
-				Process p = Runtime.getRuntime().exec("adduser  --group "  + ci.udata[i].getUserType() + " " + ci.udata[i].getUserLogin());
+				Process p = Runtime.getRuntime().exec("useradd -g "  + ci.udata[i].getUserType() + " " + ci.udata[i].getUserLogin());
 				BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
 				this.currentHostname = input.readLine();
 				input.close();
@@ -129,21 +131,67 @@ public class WanduxPost
 //		}
 	}
 
-	public void confWebPrograms()
+	public void confPrograms()
 	{
 		
 	}
 	
+	private void createLocalStorage()
+	{
+		// Linux Folder /wandux creation
+		// Linux folder /tmp/wanduxStorage creation
+		File localstorage = new File("/tmp/wanduxStorage/");
+		localstorage.mkdir();
+		// Linux folder /tmp/wanduxStorage/<hostname> creation
+		localstorage = new File("/tmp/wanduxStorage/" + this.currentHostname);
+		localstorage.mkdir();
+		
+        FileOutputStream out; // declare a file output object
+        PrintStream p; // declare a print stream object
+
+        try
+        {
+                // Create a new file output stream
+                // connected to "myfile.txt"
+                out = new FileOutputStream("/tmp/wanduxcreds");
+
+                // Connect print stream to the output stream
+                p = new PrintStream( out );
+                p.println ("username=wandux");
+                p.println ("password=wandux");
+                p.close();
+        }
+        catch (Exception e)
+        {
+                System.err.println ("Error writing to file");
+        }
+        
+		try {
+			Process smbmount = Runtime.getRuntime().exec("smbmount \\\\\\\\"
+												+ this.currentHostname
+												+ "\\\\wanduxStorage /tmp/wanduxStorage/" + this.currentHostname
+												+ " -o credentials=/tmp/wanduxcreds");
+			BufferedReader smblog = new BufferedReader(new InputStreamReader(smbmount.getInputStream()));
+			this.currentHostname = smblog.readLine();
+			smblog.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public void getDataFromStorageServer()
 	{
-		File fromServer = new File("/home/samba/wanduxStorage/" + this.currentHostname);
+		createLocalStorage();
+		
+		File fromServer = new File("/tmp/wanduxStorage/" + this.currentHostname);
 		fromServer.mkdir();
 		File f = new File("/wandux/");
 		if (f.exists() ==  false)
 			f.mkdir();
 
-		try { // TODO a changer par l url samba !!
-			Process p = Runtime.getRuntime().exec("cp -R /home/samba/wanduxStorage/" + this.currentHostname + " /wandux/");
+		try {
+			Process p = Runtime.getRuntime().exec("cp -R /tmp/wanduxStorage/" + this.currentHostname + " /wandux/");
 			BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
 			this.currentHostname = input.readLine();
 			input.close();
